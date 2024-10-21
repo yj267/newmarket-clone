4944be213ed8f38c82bc68c89da79906aeaed9b1
yj267 <uujin1023@daum.net>
2024-10-21 16:03:04 +0900

    로그인 인증 기능 추가

--- frontend/login.js
+++ frontend/login.js
@@ -1,7 +1,5 @@
 const form = document.querySelector("#login-form");
 
-let accessToken = null;
-
 const handleSubmit = async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -13,25 +11,11 @@ const handleSubmit = async (event) => {
     body: formData,
   });
   const data = await res.json();
-  accessToken = data.access_token;
-
-  const infoDiv = document.querySelector("#info");
-  infoDiv.innerText = "로그인되었습니다!";
+  const accessToken = data.access_token;
+  window.localStorage.setItem("token", accessToken);
+  alert("로그인되었습니다!");
 
   window.location.pathname = "/";
-
-  const btn = document.createElement("button");
-  btn.innerText = "상품 가져오기";
-  btn.addEventListener("click", async () => {
-    const res = await fetch("/items", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const data = await res.json();
-    console.log(data);
-  });
-  infoDiv.appendChild(btn);
 };
 
 form.addEventListener("submit", handleSubmit);
--- main.py
+++ main.py
@@ -64,7 +64,8 @@ async def create_item(image:UploadFile,
                 price:Annotated[int,Form()],
                 description:Annotated[str,Form()],
                 place:Annotated[str,Form()],
-                insetrAt:Annotated[int,Form()]
+                insetrAt:Annotated[int,Form()],
+                user=Depends(manager)
                 ):
 
         image_bytes = await image.read()
